6cfc1b5176ac9e68635a1d313883de7aea3fcc2e
Nurtilek <nurdinov.nurtilek@gmail.com>
2021-10-25 13:50:25 +0600

    Added Todos, Home pages

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from "../components/containers/Container";
+
+const HomePage: React.FC = (): React.ReactElement => {
+  return <Container>Home Page</Container>;
+};
+
+export default HomePage;
---
+++ src/pages/TodoPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Container from "src/components/containers/Container";
+
+const TodosPage: React.FC = (): React.ReactElement => {
+  return <Container>Todos Page</Container>;
+};
+
+export default TodosPage;
