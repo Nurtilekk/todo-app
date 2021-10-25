c8dbcc8a799f4d100be94e54e2eac3d11d1e67f6
Nurtilek <nurdinov.nurtilek@gmail.com>
2021-10-25 13:50:49 +0600

    General changes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+import HomePage from "src/pages/HomePage";
+import Header from "./components/containers/Header";
+import TodosPage from "./pages/TodoPage";
+import GlobalStyle from "./styles/GlobalStyles";
+
+const theme = {
+  colors: {
+    primary: "#D52047",
+    white: "#fff",
+    dark: "#000",
+    danger: "#EE3D1B",
+  },
+};
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Router>
+        <Header />
+        <Switch>
+          <Route component={HomePage} exact path="/"></Route>
+          <Route component={TodosPage} path="/todos"></Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
